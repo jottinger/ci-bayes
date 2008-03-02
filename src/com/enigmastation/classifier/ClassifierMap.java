7a02a14814e18816f7b4dde90b62de7b5fbce43b
epesh <epesh@26af8191-f038-0410-b75b-cbc95a8273bc>
2008-03-02 13:51:48 +0000

    Converted to GNU Trove, to check for memory usage - during testing, ended up with 160MB with Trove.
    git-svn-id: https://ci-bayes.dev.java.net:/svn/ci-bayes/trunk@31 26af8191-f038-0410-b75b-cbc95a8273bc

--- src/com/enigmastation/classifier/ClassifierMap.java
+++ src/com/enigmastation/classifier/ClassifierMap.java
@@ -1,21 +1,27 @@
 package com.enigmastation.classifier;
 
-import javolution.util.FastMap;
+import gnu.trove.TObjectIntHashMap;
 
-import java.util.Map;
-
-public final class ClassifierMap extends FastMap<String, Integer> implements Map<String, Integer> {
+/**
+ * @version: $Revision$
+ */
+public final class ClassifierMap extends TObjectIntHashMap<String> {
     private long totalCount;
 
     public void incrementCategory(String category) {
-        int i=0;
-        try { 
-            i=get(category).intValue()+1;
-        } catch(NullPointerException npe) {
-            i=1;
+        //       int i=0;
+//        try {
+//            i=get(category)+1;
+        //       } catch(NullPointerException npe) {
+        //          i=1;
+        //     }
+        if (this.containsKey(category)) {
+            this.increment(category);
+        } else {
+            put(category, 1);
         }
-        put(category, i);
-        totalCount+=1;
+//        put(category, i);
+        totalCount += 1;
     }
 
     public double getTotalCount() {
