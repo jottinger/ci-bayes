0bcd72c77626c8f203a97569b3df0e77d9ca8558
epesh <epesh@26af8191-f038-0410-b75b-cbc95a8273bc>
2008-06-25 19:09:31 +0000

    Getting rid of more space-specific stuff.
    git-svn-id: https://ci-bayes.dev.java.net:/svn/ci-bayes/trunk@67 26af8191-f038-0410-b75b-cbc95a8273bc

--- src/main/java/com/enigmastation/neuralnet/model/Linkage.java
+++ src/main/java/com/enigmastation/neuralnet/model/Linkage.java
@@ -1,31 +1,18 @@
 package com.enigmastation.neuralnet.model;
 
-import com.gigaspaces.annotation.pojo.SpaceProperty;
-
-/**
- * Created by IntelliJ IDEA.
- * User: joeo
- * Date: Jun 25, 2008
- * Time: 12:29:11 PM
- * To change this template use File | Settings | File Templates.
- */
 public interface Linkage {
-    @SpaceProperty
     int getLayer();
 
     void setLayer(int layer);
 
-    @SpaceProperty
     Integer getFromId();
 
     void setFromId(Integer fromId);
 
-    @SpaceProperty
     Integer getToId();
 
     void setToId(Integer toId);
 
-    @SpaceProperty
     Double getStrength();
 
     void setStrength(Double strength);
