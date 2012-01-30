55c250eb8efbad79ebea915a1e2ff471f22c744a
U-osiris\joeo <joeo@osiris.(none)>
2012-01-30 17:00:06 -0500

    FisherClassifier tests working

--- bayes/src/test/java/com/enigmastation/ml/bayes/impl/ClassifierTest.java
+++ bayes/src/test/java/com/enigmastation/ml/bayes/impl/ClassifierTest.java
@@ -5,78 +5,84 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class ClassifierTest {
-  @Test
-  public void testInternalsOfTrain() {
-    SimpleClassifier c = new SimpleClassifier();
-    c.train("the quick brown fox jumps over the lazy dog's tail", "good");
-    c.train("make quick money in the online casino", "bad");
-    assertEquals(c.fcount("quick", "good"), 1);
-    assertEquals(c.fcount("quick", "bad"), 1);
-  }
-
-  @Test
-  public void testfprob() {
-    SimpleClassifier cl = getTrainedClassifier();
-    assertEquals(cl.fprob("quick", "good"), 0.6666, 0.0001);
-    assertEquals(cl.weightedprob("monei", "good"), 0.25, 0.001);
-    train(cl);
-    assertEquals(cl.weightedprob("monei", "good"), 0.166, 0.001);
-  }
-
-  @Test
-  public void testBayes() {
-    SimpleClassifier cl = getTrainedClassifier();
-    assertEquals(cl.prob("quick rabbit", "good"), 0.15624, 0.0001);
-    assertEquals(cl.prob("quick rabbit", "bad"), 0.05, 0.001);
-  }
-
-  @Test
-  public void testThreshold() {
-    SimpleClassifier cl = getTrainedClassifier();
-    assertEquals(cl.getThreshold("bad"), 1.0, 0.01);
-  }
-
-  @Test
-  public void testClassification() {
-    SimpleClassifier cl = getTrainedClassifier();
-    assertEquals(cl.classify("quick rabbit", "unknown"), "good");
-    assertEquals(cl.classify("quick money", "unknown"), "bad");
-    cl.setThreshold("bad", 3.0);
-    assertEquals(cl.classify("quick money", "unknown"), "unknown");
-    for (int i = 0; i < 10; i++) {
-      train(cl);
+    @Test
+    public void testInternalsOfTrain() {
+        SimpleClassifier c = new SimpleClassifier();
+        c.train("the quick brown fox jumps over the lazy dog's tail", "good");
+        c.train("make quick money in the online casino", "bad");
+        assertEquals(c.fcount("quick", "good"), 1);
+        assertEquals(c.fcount("quick", "bad"), 1);
     }
-    assertEquals(cl.classify("quick money", "unknown"), "bad");
-  }
 
-  private SimpleClassifier getTrainedClassifier() {
-    SimpleClassifier cl = new SimpleClassifier();
+    @Test
+    public void testfprob() {
+        SimpleClassifier cl = getTrainedClassifier();
+        assertEquals(cl.fprob("quick", "good"), 0.6666, 0.0001);
+        assertEquals(cl.weightedprob("monei", "good"), 0.25, 0.001);
+        train(cl);
+        assertEquals(cl.weightedprob("monei", "good"), 0.166, 0.001);
+    }
 
-    train(cl);
-    return cl;
-  }
+    @Test
+    public void testBayes() {
+        SimpleClassifier cl = getTrainedClassifier();
+        assertEquals(cl.prob("quick rabbit", "good"), 0.15624, 0.0001);
+        assertEquals(cl.prob("quick rabbit", "bad"), 0.05, 0.001);
+    }
 
-  private void train(SimpleClassifier cl) {
-    cl.train("Nobody owns the water", "good");
-    cl.train("The quick rabbit jumps fences", "good");
-    cl.train("Buy pharmaceuticals now", "bad");
-    cl.train("make quick money in the online casino", "bad");
-    cl.train("the quick brown fox jumps", "good");
-  }
-  
-  private FisherClassifier getTrainedFisher() {
-    FisherClassifier fc=new FisherClassifier();
-    train(fc);
-    return fc;
-  }
-  
-  @Test
-  public void testFisherCProb() {
-    FisherClassifier cl=getTrainedFisher();
+    @Test
+    public void testThreshold() {
+        SimpleClassifier cl = getTrainedClassifier();
+        assertEquals(cl.getThreshold("bad"), 1.0, 0.01);
+    }
 
-    assertEquals(cl.fprob("quick", "good"), 0.571,0.001);
-    assertEquals(cl.fprob("monei", "bad"), 1.0, 0.001);
-    assertEquals(cl.fisherprob("quick rabbit", "good"), 0.78, 0.001);
-    assertEquals(cl.fisherprob("quick rabbit", "bad"), 0.356, 0.001);
-  }
+    @Test
+    public void testClassification() {
+        SimpleClassifier cl = getTrainedClassifier();
+        assertEquals(cl.classify("quick rabbit", "unknown"), "good");
+        assertEquals(cl.classify("quick money", "unknown"), "bad");
+        cl.setThreshold("bad", 3.0);
+        assertEquals(cl.classify("quick money", "unknown"), "unknown");
+        for (int i = 0; i < 10; i++) {
+            train(cl);
+        }
+        assertEquals(cl.classify("quick money", "unknown"), "bad");
+    }
+
+    private SimpleClassifier getTrainedClassifier() {
+        SimpleClassifier cl = new SimpleClassifier();
+
+        train(cl);
+        return cl;
+    }
+
+    private void train(SimpleClassifier cl) {
+        cl.train("Nobody owns the water", "good");
+        cl.train("The quick rabbit jumps fences", "good");
+        cl.train("Buy pharmaceuticals now", "bad");
+        cl.train("make quick money in the online casino", "bad");
+        cl.train("the quick brown fox jumps", "good");
+    }
+
+    private FisherClassifier getTrainedFisher() {
+        FisherClassifier fc = new FisherClassifier();
+        train(fc);
+        return fc;
+    }
+
+    @Test
+    public void testFisherCProb() {
+        FisherClassifier cl = getTrainedFisher();
+
+        assertEquals(cl.fprob("quick", "good"), 0.571, 0.001);
+        assertEquals(cl.fprob("monei", "bad"), 1.0, 0.001);
+        assertEquals(cl.fisherprob("quick rabbit", "good"), 0.78, 0.001);
+        assertEquals(cl.fisherprob("quick rabbit", "bad"), 0.356, 0.001);
+        assertEquals(cl.classify("quick rabbit"), "good");
+        assertEquals(cl.classify("quick money"), "bad");
+        cl.setMinimum("bad", 0.8);
+        assertEquals(cl.classify("quick money"), "good");
+        cl.setMinimum("bad", 0.4);
+        assertEquals(cl.classify("quick money"), "bad");
+    }
 }
