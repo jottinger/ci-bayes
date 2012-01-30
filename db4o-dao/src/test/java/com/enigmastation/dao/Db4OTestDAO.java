5c2eb7a9a3c9a14b59a148d7ecb42f3a2de54173
U-osiris\joeo <joeo@osiris.(none)>
2012-01-30 14:48:44 -0500

    Updating POM and validating a few minor things, eh

--- db4o-dao/src/test/java/com/enigmastation/dao/Db4OTestDAO.java
+++
@@ -1,8 +0,0 @@
-package com.enigmastation.dao;
-
-import com.enigmastation.dao.db4o.Db4OBaseDAO;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class Db4OTestDAO extends Db4OBaseDAO<TestEntity> {
-}
