1631a2f1553779b71da71ab1eba10cc0c81abb64
Joseph Ottinger <joeo@enigmastation.com>
2012-01-31 10:58:31 -0500

    adding cassandra shell project

---
+++ cassandra-persistence/src/main/java/com/enigmastation/ml/bayes/persistence/CassandraDataStore.java
@@ -0,0 +1,11 @@
+package com.enigmastation.ml.bayes.persistence;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joeo
+ * Date: 1/31/12
+ * Time: 10:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CassandraDataStore {
+}
---
+++ cassandra-persistence/src/test/java/com/enigmastation/ml/bayes/persistence/CassandraPersistenceTest.java
@@ -0,0 +1,11 @@
+package com.enigmastation.ml.bayes.persistence;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joeo
+ * Date: 1/31/12
+ * Time: 10:50 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CassandraPersistenceTest {
+}
