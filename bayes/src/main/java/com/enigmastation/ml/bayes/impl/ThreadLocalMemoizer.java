f82f266db92e4bcf9790c46e4c614a931c03795d
U-osiris\joeo <joeo@osiris.(none)>
2012-01-31 10:15:16 -0500

    Adding corpus tests, which take roughly 35 minutes to run, but which pass

---
+++ bayes/src/main/java/com/enigmastation/ml/bayes/impl/ThreadLocalMemoizer.java
@@ -0,0 +1,11 @@
+package com.enigmastation.ml.bayes.impl;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joeo
+ * Date: 1/31/12
+ * Time: 9:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ThreadLocalMemoizer {
+}
---
+++ bayes/src/test/java/com/enigmastation/ml/bayes/CorpusTest.java
@@ -0,0 +1,11 @@
+package com.enigmastation.ml.bayes;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joeo
+ * Date: 1/31/12
+ * Time: 6:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CorpusTest {
+}
