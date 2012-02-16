dcd9c4192dbae1db95899c752c1fecb53bc38f2e
Joseph Ottinger <jottinge@jottinge.csb>
2012-02-16 12:37:22 -0500

    Fixing broken persistAll() implementation in datastore

--- common/src/main/java/com/enigmastation/ml/common/collections/MapWithDefaultValue.java
+++ common/src/main/java/com/enigmastation/ml/common/collections/MapWithDefaultValue.java
@@ -5,72 +5,73 @@ import org.cliffc.high_scale_lib.NonBlockingHashMap;
 import java.util.Map;
 
 public class MapWithDefaultValue<K, V>
-extends NonBlockingHashMap<K, V> {
-  ValueProvider<K, V> valueProvider;
-  Map<K, V> updates = new NonBlockingHashMap<>();
-  int syncBlockSize = 0;
-  MapDataStore<K, V> dataStore;
+        extends NonBlockingHashMap<K, V> {
+    ValueProvider<K, V> valueProvider;
+    Map<K, V> updates = new NonBlockingHashMap<>();
+    int syncBlockSize = 0;
+    MapDataStore<K, V> dataStore;
 
-  public void init() {
-    Map<K, V> load = valueProvider.load();
-    if (load != null) {
-      this.putAll(load);
+    public void init() {
+        Map<K, V> load = valueProvider.load();
+        if (load != null) {
+            this.putAll(load);
+        }
     }
-  }
 
-  public int getSyncBlockSize() {
-    return syncBlockSize;
-  }
-
-  public void setSyncBlockSize(int syncBlockSize) {
-    this.syncBlockSize = syncBlockSize;
-    checkForSync();
-  }
-
-  public MapDataStore<K, V> getDataStore() {
-    return dataStore;
-  }
-
-  public void setDataStore(MapDataStore<K, V> dataStore) {
-    this.dataStore = dataStore;
-  }
-
-  public MapWithDefaultValue(ValueProvider<K, V> valueProvider) {
-    this.valueProvider = valueProvider;
-  }
-
-  public MapWithDefaultValue(int initial_sz, ValueProvider<K, V> valueProvider) {
-    super(initial_sz);
-    this.valueProvider = valueProvider;
-  }
-
-  public V get(Object key) {
-    V object = super.get(key);
-    if (object == null) {
-      object = valueProvider.getDefault(key);
+    public int getSyncBlockSize() {
+        return syncBlockSize;
     }
-    return object;
-  }
 
-  @Override
-  public V put(K key, V val) {
-    updates.put(key, val);
-    checkForSync();
-    return super.put(key, val);
-  }
-
-  private void checkForSync() {
-    if (updates.size() > syncBlockSize) {
-      synchronize();
+    public void setSyncBlockSize(int syncBlockSize) {
+        this.syncBlockSize = syncBlockSize;
+        checkForSync();
     }
-  }
 
-  public void synchronize() {
-    if (!dataStore.persistAll(this)) {
-      dataStore.persist(updates);
-    } else {
-      // since persistAll() says it saved everything, just empty the journal
-      updates.clear();
+    public MapDataStore<K, V> getDataStore() {
+        return dataStore;
     }
-  }
+
+    public void setDataStore(MapDataStore<K, V> dataStore) {
+        this.dataStore = dataStore;
+    }
+
+    public MapWithDefaultValue(ValueProvider<K, V> valueProvider) {
+        this.valueProvider = valueProvider;
+    }
+
+    public MapWithDefaultValue(int initial_sz, ValueProvider<K, V> valueProvider) {
+        super(initial_sz);
+        this.valueProvider = valueProvider;
+    }
+
+    public V get(Object key) {
+        V object = super.get(key);
+        if (object == null) {
+            object = valueProvider.getDefault(key);
+        }
+        return object;
+    }
+
+    @Override
+    public V put(K key, V val) {
+        updates.put(key, val);
+        checkForSync();
+        return super.put(key, val);
+    }
+
+    private void checkForSync() {
+        if (updates.size() > syncBlockSize) {
+            synchronize();
+        }
+    }
+
+    public void synchronize() {
+        boolean t = dataStore.persistAll(this);
+        if (t) {
+            dataStore.persist(updates);
+        } else {
+            // since persistAll() says it saved everything, just empty the journal
+            updates.clear();
+        }
+    }
 }
